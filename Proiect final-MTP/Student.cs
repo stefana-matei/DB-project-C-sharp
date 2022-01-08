ed893814cb12722ec2f7cb2a5e691a908a4ae069
Stefana Matei <stefana.otilia.matei@gmail.com>
2022-01-08 15:35:05 +0200

    Create Student.cs

---
+++ Proiect final-MTP/Student.cs
@@ -0,0 +1,38 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Proiect_final_MTP
+{
+    class Student
+    {
+        static MySqlConnection sqlConnection = Connection.getSqlConnection();
+        static string nume, prenume, nr_legitimatie;
+
+        static public string Nume { get => nume; set => nume = value; }
+        static public string Prenume { get => prenume; set => prenume = value; }
+        static public string Legitimatie { get => nr_legitimatie; set => nr_legitimatie = value; }
+
+
+        // metoda ce returneaza numele intreg al studentului
+        static public string FullName()
+        {
+            return nume + " " + prenume;
+        }
+
+
+        // metoda ce returneaza anul de studiu al studentului
+        static public int AnStudiu()
+        {
+            string queryAnStudiu =
+               " SELECT MAX(note.an_studiu)" +
+               " FROM note" +
+               " WHERE nr_legitimatie = '" + Student.Legitimatie + "'";
+
+            sqlConnection.Open();
+            MySqlCommand sqlCommand = new MySqlCommand(queryAnStudiu, sqlConnection);
+            int anStudiu = (int)sqlCommand.ExecuteScalar();
+            sqlConnection.Close();
+
+            return anStudiu;
+        }
+    }
+}
