ddebe7c97627e0a9f841056510017a015db3c875
Stefana Matei <stefana.otilia.matei@gmail.com>
2022-01-08 16:52:35 +0200

    Update Documente.Designer.cs

--- Proiect final-MTP/Documente.Designer.cs
+++ Proiect final-MTP/Documente.Designer.cs
@@ -29,10 +29,72 @@ namespace Proiect_final_MTP
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.lblRaportDetaliat = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnGenerareDocument = new FontAwesome.Sharp.IconButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblRaportDetaliat
+            // 
+            this.lblRaportDetaliat.AutoSize = true;
+            this.lblRaportDetaliat.Font = new System.Drawing.Font("Lucida Bright", 13.8F, System.Drawing.FontStyle.Bold);
+            this.lblRaportDetaliat.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblRaportDetaliat.Location = new System.Drawing.Point(155, 96);
+            this.lblRaportDetaliat.Name = "lblRaportDetaliat";
+            this.lblRaportDetaliat.Size = new System.Drawing.Size(822, 27);
+            this.lblRaportDetaliat.TabIndex = 28;
+            this.lblRaportDetaliat.Text = "Raport detaliat - lista disciplinelor contractate (promovate, restante)";
+            this.lblRaportDetaliat.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Proiect_final_MTP.Properties.Resources.icons8_export_pdf_40;
+            this.pictureBox1.Location = new System.Drawing.Point(100, 86);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(38, 46);
+            this.pictureBox1.TabIndex = 29;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnGenerareDocument
+            // 
+            this.btnGenerareDocument.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(194)))));
+            this.btnGenerareDocument.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGenerareDocument.FlatAppearance.BorderSize = 2;
+            this.btnGenerareDocument.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGenerareDocument.Font = new System.Drawing.Font("Lucida Bright", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerareDocument.ForeColor = System.Drawing.Color.Snow;
+            this.btnGenerareDocument.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.btnGenerareDocument.IconColor = System.Drawing.Color.Black;
+            this.btnGenerareDocument.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnGenerareDocument.Location = new System.Drawing.Point(160, 152);
+            this.btnGenerareDocument.Name = "btnGenerareDocument";
+            this.btnGenerareDocument.Size = new System.Drawing.Size(109, 37);
+            this.btnGenerareDocument.TabIndex = 26;
+            this.btnGenerareDocument.Text = "Generare ";
+            this.btnGenerareDocument.UseVisualStyleBackColor = false;
+            this.btnGenerareDocument.Click += new System.EventHandler(this.btnGenerareDocument_Click);
+            // 
+            // Documente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblRaportDetaliat);
+            this.Controls.Add(this.btnGenerareDocument);
+            this.Name = "Documente";
+            this.Size = new System.Drawing.Size(1378, 600);
+            this.Load += new System.EventHandler(this.Documente_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private FontAwesome.Sharp.IconButton btnGenerareDocument;
+        private System.Windows.Forms.Label lblRaportDetaliat;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
