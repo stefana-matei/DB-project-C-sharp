065b7fbf93499ed13c0be65363b4521116e10d6c
Stefana Matei <stefana.otilia.matei@gmail.com>
2022-01-08 16:43:19 +0200

    Update Restante UserControl

--- Proiect final-MTP/Restante.Designer.cs
+++ Proiect final-MTP/Restante.Designer.cs
@@ -29,10 +29,83 @@ namespace Proiect_final_MTP
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgvRestante = new System.Windows.Forms.DataGridView();
+            this.cmbAnUniversitar = new System.Windows.Forms.ComboBox();
+            this.lblRestante = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRestante)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvRestante
+            // 
+            this.dgvRestante.AllowUserToAddRows = false;
+            this.dgvRestante.AllowUserToDeleteRows = false;
+            this.dgvRestante.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvRestante.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvRestante.BackgroundColor = System.Drawing.Color.Azure;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(194)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Snow;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvRestante.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvRestante.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRestante.EnableHeadersVisualStyles = false;
+            this.dgvRestante.Location = new System.Drawing.Point(172, 228);
+            this.dgvRestante.MultiSelect = false;
+            this.dgvRestante.Name = "dgvRestante";
+            this.dgvRestante.RowHeadersVisible = false;
+            this.dgvRestante.RowHeadersWidth = 51;
+            this.dgvRestante.RowTemplate.Height = 24;
+            this.dgvRestante.Size = new System.Drawing.Size(1046, 135);
+            this.dgvRestante.TabIndex = 26;
+            // 
+            // cmbAnUniversitar
+            // 
+            this.cmbAnUniversitar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cmbAnUniversitar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbAnUniversitar.FormattingEnabled = true;
+            this.cmbAnUniversitar.Location = new System.Drawing.Point(120, 148);
+            this.cmbAnUniversitar.Name = "cmbAnUniversitar";
+            this.cmbAnUniversitar.Size = new System.Drawing.Size(50, 37);
+            this.cmbAnUniversitar.TabIndex = 25;
+            this.cmbAnUniversitar.SelectedIndexChanged += new System.EventHandler(this.cmbAnUniversitar_SelectedIndexChanged);
+            // 
+            // lblRestante
+            // 
+            this.lblRestante.AutoSize = true;
+            this.lblRestante.Font = new System.Drawing.Font("Lucida Bright", 13.8F, System.Drawing.FontStyle.Bold);
+            this.lblRestante.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblRestante.Location = new System.Drawing.Point(115, 107);
+            this.lblRestante.Name = "lblRestante";
+            this.lblRestante.Size = new System.Drawing.Size(310, 27);
+            this.lblRestante.TabIndex = 24;
+            this.lblRestante.Text = "Selectati anul universitar";
+            this.lblRestante.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Restante
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dgvRestante);
+            this.Controls.Add(this.cmbAnUniversitar);
+            this.Controls.Add(this.lblRestante);
+            this.Name = "Restante";
+            this.Size = new System.Drawing.Size(1378, 600);
+            this.Load += new System.EventHandler(this.Restante_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRestante)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvRestante;
+        private System.Windows.Forms.ComboBox cmbAnUniversitar;
+        private System.Windows.Forms.Label lblRestante;
     }
 }
--- Proiect final-MTP/Restante.cs
+++ Proiect final-MTP/Restante.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,107 @@ namespace Proiect_final_MTP
 {
     public partial class Restante : UserControl
     {
+        MySqlConnection sqlConnection = Connection.getSqlConnection();
+
         public Restante()
         {
             InitializeComponent();
+        }
+
+
+        // incarcare User Control
+        private void Restante_Load(object sender, System.EventArgs e)
+        {
+            incarcareAniUniversitari();
+        }
+
+
+        // incarcare in combobox a anilor universitari ale studentului
+        private void incarcareAniUniversitari()
+        {
+            try
+            {
+                sqlConnection.Open();
+
+                // selecteaza anii de studiu pentru studentul conectat din tabela note
+                string queryAnStudiu =
+                    " SELECT DISTINCT an_studiu" +
+                    " FROM note" +
+                    " WHERE nr_legitimatie = '" + Student.Legitimatie + "'";
+
+
+                MySqlDataAdapter dataAdapter = new MySqlDataAdapter();
+                dataAdapter.SelectCommand = new MySqlCommand();
+                dataAdapter.SelectCommand.Connection = sqlConnection;
+                dataAdapter.SelectCommand.CommandText = queryAnStudiu;
+
+                DataTable dataTable = new DataTable();
+
+                dataAdapter.Fill(dataTable);
+
+                // adaugare in combobox-ul cmbNrLegitimatie nr. de legitimatie pentru fiecare student din BD
+                for (int i = 0; i < dataTable.Rows.Count; i++)
+                {
+                    cmbAnUniversitar.Items.Add(dataTable.Rows[i]["an_studiu"]);
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            sqlConnection.Close();
+        }
+
+
+        // afisare discipline nepromovate ale studentului 
+        private void cmbAnUniversitar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblRestante.Text = "Lista disciplinelor nepromovate in anul " + cmbAnUniversitar.Text;
+
+            string query =
+                " SELECT nr_legitimatie," +
+                "        disciplina," +
+                "        an_studiu," +
+                "        MAX(nota) AS nota_finala" +
+                " FROM note" +
+                " WHERE note.nr_legitimatie = '" + Student.Legitimatie + "'" +
+                "   AND note.an_studiu = '" + Int16.Parse(cmbAnUniversitar.Text) + "'" +
+                " GROUP BY disciplina" +
+                " HAVING nota_finala < 5" +
+                " ORDER BY disciplina";
+
+            connectToDataBase(query, dgvRestante);
+        }
+
+
+        // metoda pentru a afisa in DataGridView datele din BD
+        private void connectToDataBase(string query, DataGridView dataGridView)
+        {
+            try
+            {
+                sqlConnection.Open();
+
+                MySqlDataAdapter dataAdapter = new MySqlDataAdapter();
+                dataAdapter.SelectCommand = new MySqlCommand(query, sqlConnection);
+
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+
+                BindingSource bindingSource = new BindingSource();
+                bindingSource.DataSource = dataTable;
+
+                dataGridView.DataSource = bindingSource;
+
+                dataTable.Dispose();
+                dataAdapter.Dispose();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            sqlConnection.Close();
         }
     }
 }
