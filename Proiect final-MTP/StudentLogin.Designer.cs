f15a6318c30edeb6b6c523aac0e6a8793025f663
Stefana Matei <stefana.otilia.matei@gmail.com>
2022-01-08 15:28:17 +0200

    EDIT StudentLogin Form

--- Proiect final-MTP/StudentLogin.Designer.cs
+++ Proiect final-MTP/StudentLogin.Designer.cs
@@ -29,12 +29,260 @@ namespace Proiect_final_MTP
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.panelTitle = new System.Windows.Forms.Panel();
+            this.btnMinimize = new System.Windows.Forms.Button();
+            this.btnMaximize = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnRestore = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnClearFields = new FontAwesome.Sharp.IconButton();
+            this.lblAdminLogin = new System.Windows.Forms.Label();
+            this.lblStudentLogin = new System.Windows.Forms.Label();
+            this.panelTitle.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelTitle
+            // 
+            this.panelTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(194)))));
+            this.panelTitle.Controls.Add(this.btnMinimize);
+            this.panelTitle.Controls.Add(this.btnMaximize);
+            this.panelTitle.Controls.Add(this.btnCancel);
+            this.panelTitle.Controls.Add(this.btnRestore);
+            this.panelTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitle.ForeColor = System.Drawing.Color.Black;
+            this.panelTitle.Location = new System.Drawing.Point(0, 0);
+            this.panelTitle.Name = "panelTitle";
+            this.panelTitle.Size = new System.Drawing.Size(601, 58);
+            this.panelTitle.TabIndex = 2;
+            this.panelTitle.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelTitle_MouseDown);
+            this.panelTitle.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelTitle_MouseMove);
+            this.panelTitle.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panelTitle_MouseUp);
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMinimize.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(194)))));
+            this.btnMinimize.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMinimize.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnMinimize.FlatAppearance.BorderSize = 0;
+            this.btnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinimize.ForeColor = System.Drawing.Color.Transparent;
+            this.btnMinimize.Image = global::Proiect_final_MTP.Properties.Resources.minimize3;
+            this.btnMinimize.Location = new System.Drawing.Point(437, 24);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(57, 30);
+            this.btnMinimize.TabIndex = 12;
+            this.btnMinimize.UseVisualStyleBackColor = false;
+            this.btnMinimize.Click += new System.EventHandler(this.btnMinimize_Click);
+            // 
+            // btnMaximize
+            // 
+            this.btnMaximize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMaximize.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(194)))));
+            this.btnMaximize.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMaximize.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnMaximize.FlatAppearance.BorderSize = 0;
+            this.btnMaximize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMaximize.ForeColor = System.Drawing.Color.Transparent;
+            this.btnMaximize.Image = global::Proiect_final_MTP.Properties.Resources.maximize3;
+            this.btnMaximize.Location = new System.Drawing.Point(481, 24);
+            this.btnMaximize.Name = "btnMaximize";
+            this.btnMaximize.Size = new System.Drawing.Size(57, 30);
+            this.btnMaximize.TabIndex = 11;
+            this.btnMaximize.UseVisualStyleBackColor = false;
+            this.btnMaximize.Click += new System.EventHandler(this.btnMaximize_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(194)))));
+            this.btnCancel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.FlatAppearance.BorderSize = 0;
+            this.btnCancel.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.ForeColor = System.Drawing.Color.Transparent;
+            this.btnCancel.Image = global::Proiect_final_MTP.Properties.Resources.close3;
+            this.btnCancel.Location = new System.Drawing.Point(527, 24);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(57, 30);
+            this.btnCancel.TabIndex = 10;
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnRestore
+            // 
+            this.btnRestore.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRestore.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(194)))));
+            this.btnRestore.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRestore.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnRestore.FlatAppearance.BorderSize = 0;
+            this.btnRestore.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRestore.ForeColor = System.Drawing.Color.Transparent;
+            this.btnRestore.Image = global::Proiect_final_MTP.Properties.Resources.restore3;
+            this.btnRestore.Location = new System.Drawing.Point(365, 23);
+            this.btnRestore.Name = "btnRestore";
+            this.btnRestore.Size = new System.Drawing.Size(57, 30);
+            this.btnRestore.TabIndex = 13;
+            this.btnRestore.UseVisualStyleBackColor = false;
+            this.btnRestore.Visible = false;
+            this.btnRestore.Click += new System.EventHandler(this.btnRestore_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(194)))));
+            this.btnLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(205, 604);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(181, 51);
+            this.btnLogin.TabIndex = 39;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(80, 501);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(436, 33);
+            this.txtPassword.TabIndex = 38;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblPassword.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblPassword.Location = new System.Drawing.Point(75, 460);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(112, 27);
+            this.lblPassword.TabIndex = 37;
+            this.lblPassword.Text = "Password*";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(80, 376);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(436, 33);
+            this.txtEmail.TabIndex = 36;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblEmail.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblEmail.Location = new System.Drawing.Point(75, 335);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(72, 27);
+            this.lblEmail.TabIndex = 35;
+            this.lblEmail.Text = "Email*";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Proiect_final_MTP.Properties.Resources.icons8_student_100__1_;
+            this.pictureBox1.Location = new System.Drawing.Point(252, 115);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(101, 106);
+            this.pictureBox1.TabIndex = 41;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnClearFields
+            // 
+            this.btnClearFields.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnClearFields.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClearFields.FlatAppearance.BorderSize = 0;
+            this.btnClearFields.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClearFields.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClearFields.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(194)))));
+            this.btnClearFields.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.btnClearFields.IconColor = System.Drawing.Color.Black;
+            this.btnClearFields.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnClearFields.Location = new System.Drawing.Point(399, 569);
+            this.btnClearFields.Name = "btnClearFields";
+            this.btnClearFields.Size = new System.Drawing.Size(134, 30);
+            this.btnClearFields.TabIndex = 40;
+            this.btnClearFields.Text = "Clear Fields";
+            this.btnClearFields.UseVisualStyleBackColor = false;
+            this.btnClearFields.Click += new System.EventHandler(this.btnClearFields_Click);
+            // 
+            // lblAdminLogin
+            // 
+            this.lblAdminLogin.AutoSize = true;
+            this.lblAdminLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblAdminLogin.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAdminLogin.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblAdminLogin.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblAdminLogin.Location = new System.Drawing.Point(233, 662);
+            this.lblAdminLogin.Name = "lblAdminLogin";
+            this.lblAdminLogin.Size = new System.Drawing.Size(131, 23);
+            this.lblAdminLogin.TabIndex = 42;
+            this.lblAdminLogin.Text = "Sunteti admin?";
+            this.lblAdminLogin.Click += new System.EventHandler(this.lblAdminLogin_Click);
+            // 
+            // lblStudentLogin
+            // 
+            this.lblStudentLogin.AutoSize = true;
+            this.lblStudentLogin.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStudentLogin.Location = new System.Drawing.Point(199, 240);
+            this.lblStudentLogin.Name = "lblStudentLogin";
+            this.lblStudentLogin.Size = new System.Drawing.Size(212, 37);
+            this.lblStudentLogin.TabIndex = 43;
+            this.lblStudentLogin.Text = "Login student";
+            // 
+            // StudentLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(601, 732);
+            this.Controls.Add(this.lblStudentLogin);
+            this.Controls.Add(this.lblAdminLogin);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnClearFields);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.panelTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "StudentLogin";
             this.Text = "StudentLogin";
+            this.panelTitle.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelTitle;
+        private System.Windows.Forms.Button btnMinimize;
+        private System.Windows.Forms.Button btnMaximize;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnRestore;
+        private FontAwesome.Sharp.IconButton btnClearFields;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblAdminLogin;
+        private System.Windows.Forms.Label lblStudentLogin;
     }
 }
--- Proiect final-MTP/StudentLogin.cs
+++ Proiect final-MTP/StudentLogin.cs
@@ -1,20 +1,190 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Proiect_final_MTP
 {
     public partial class StudentLogin : Form
     {
+
+        public bool mouseDown;
+        public bool maximize;
+        public bool restore;
+        Point lastLocation;
+
+        MySqlConnection sqlConnection = Connection.getSqlConnection();
+
         public StudentLogin()
         {
             InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
         }
+
+
+        // functionalitate buton de login din Form
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtEmail.Text.Equals("") && txtPassword.Text.Equals(""))
+                {
+                    MessageBox.Show("Campurile nu sunt completate, mai incearca!", "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    // daca adresa de email introdusa este valida
+                    if (isEmail(txtEmail.Text))
+                    {
+                        login();
+                    }
+                    else
+                        MessageBox.Show("Adresa de email nu are formatul valid!", "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        // metoda prin care se realizeaza conexiunea unui student existent in aplicatie
+        private void login()
+        {
+            string queryAdmin =
+                "SELECT *" +
+                " FROM login_student" +
+                " WHERE email = @email" +
+                "   AND password = @password";
+
+
+            // prepare the connection
+            MySqlCommand sqlCommand = sqlConnection.CreateCommand();
+            sqlCommand.Parameters.AddWithValue("@email", txtEmail.Text);
+            sqlCommand.Parameters.AddWithValue("@password", txtPassword.Text);
+            sqlCommand.CommandText = queryAdmin;
+            MySqlDataReader dataReader;
+
+
+            try
+            {
+                sqlConnection.Open();
+                dataReader = sqlCommand.ExecuteReader();
+
+                if (dataReader.HasRows)
+                {
+                    while (dataReader.Read())
+                    {
+                        Student.Legitimatie = dataReader.GetString(1);
+                        Student.Nume = dataReader.GetString(2);
+                        Student.Prenume = dataReader.GetString(3);
+
+                        new StudentForm().Show();
+                        this.Hide();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Student inexistent sau datele introduse sunt gresite!", "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+            sqlConnection.Close();
+        }
+
+
+        // validare adresa de email
+        public static bool isEmail(string email)
+        {
+            if (Regex.IsMatch(email, @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$"))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+
+        // stergerea textului din textbox-uri( clear fields)
+        private void btnClearFields_Click(object sender, EventArgs e)
+        {
+            txtEmail.Text = "";
+            txtPassword.Text = "";
+        }
+
+
+
+        // accesare form pentru conectare Admin
+        private void lblAdminLogin_Click(object sender, EventArgs e)
+        {
+            Login login = new Login();
+            login.Show();
+            this.Hide();
+        }
+
+
+        // ---------------------butoane pentru Form-ul StudentLogin---------------------
+        // buton restore
+        private void btnRestore_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            btnRestore.Visible = false;
+            btnMaximize.Location = btnRestore.Location;
+            btnMaximize.Visible = true;
+        }
+
+
+        //buton pentru minimize
+        private void btnMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+            //maximize = false;
+        }
+
+
+        // buton pentru maximize
+        private void btnMaximize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            btnMaximize.Visible = false;
+            btnRestore.Location = btnMaximize.Location;
+            btnRestore.Visible = true;
+        }
+
+
+        // buton de cancel
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        private void panelTitle_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            lastLocation = e.Location;
+        }
+
+
+        private void panelTitle_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                this.Location = new Point((this.Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y);
+                this.Update();
+            }
+        }
+
+
+        private void panelTitle_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+
+        // -----------------------------------------------------------------------------
     }
 }
