fae775fab747fd8d79960e8b506e7d863d916b8c
Stefana Matei <stefana.otilia.matei@gmail.com>
2022-01-08 16:30:50 +0200

    ADD AdaugareNota UserControl

---
+++ Proiect final-MTP/AdaugareNota.Designer.cs
@@ -0,0 +1,259 @@
+﻿
+namespace Proiect_final_MTP
+{
+    partial class AdaugareNota
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblLegitimatie = new System.Windows.Forms.Label();
+            this.cmbNrLegitimatie = new System.Windows.Forms.ComboBox();
+            this.lblDisciplina = new System.Windows.Forms.Label();
+            this.lblAnStudiu = new System.Windows.Forms.Label();
+            this.txtAnStudiu = new System.Windows.Forms.TextBox();
+            this.lblNrPrezentare = new System.Windows.Forms.Label();
+            this.txtNrPrezentare = new System.Windows.Forms.TextBox();
+            this.lblData = new System.Windows.Forms.Label();
+            this.dtpData = new System.Windows.Forms.DateTimePicker();
+            this.lblNota = new System.Windows.Forms.Label();
+            this.txtNotaStudent = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.cmbDiscipline = new System.Windows.Forms.ComboBox();
+            this.btnAdaugareNota = new FontAwesome.Sharp.IconButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblLegitimatie
+            // 
+            this.lblLegitimatie.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblLegitimatie.AutoSize = true;
+            this.lblLegitimatie.Font = new System.Drawing.Font("Georgia", 12F);
+            this.lblLegitimatie.Location = new System.Drawing.Point(82, 186);
+            this.lblLegitimatie.Name = "lblLegitimatie";
+            this.lblLegitimatie.Size = new System.Drawing.Size(212, 24);
+            this.lblLegitimatie.TabIndex = 5;
+            this.lblLegitimatie.Text = "Nr. legitimatie student";
+            // 
+            // cmbNrLegitimatie
+            // 
+            this.cmbNrLegitimatie.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.cmbNrLegitimatie.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F);
+            this.cmbNrLegitimatie.FormattingEnabled = true;
+            this.cmbNrLegitimatie.Location = new System.Drawing.Point(300, 186);
+            this.cmbNrLegitimatie.Name = "cmbNrLegitimatie";
+            this.cmbNrLegitimatie.Size = new System.Drawing.Size(84, 28);
+            this.cmbNrLegitimatie.TabIndex = 6;
+            // 
+            // lblDisciplina
+            // 
+            this.lblDisciplina.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblDisciplina.AutoSize = true;
+            this.lblDisciplina.Font = new System.Drawing.Font("Georgia", 12F);
+            this.lblDisciplina.Location = new System.Drawing.Point(82, 254);
+            this.lblDisciplina.Name = "lblDisciplina";
+            this.lblDisciplina.Size = new System.Drawing.Size(100, 24);
+            this.lblDisciplina.TabIndex = 7;
+            this.lblDisciplina.Text = "Disciplina";
+            // 
+            // lblAnStudiu
+            // 
+            this.lblAnStudiu.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblAnStudiu.AutoSize = true;
+            this.lblAnStudiu.Font = new System.Drawing.Font("Georgia", 12F);
+            this.lblAnStudiu.Location = new System.Drawing.Point(82, 317);
+            this.lblAnStudiu.Name = "lblAnStudiu";
+            this.lblAnStudiu.Size = new System.Drawing.Size(97, 24);
+            this.lblAnStudiu.TabIndex = 9;
+            this.lblAnStudiu.Text = "An studiu";
+            // 
+            // txtAnStudiu
+            // 
+            this.txtAnStudiu.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtAnStudiu.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAnStudiu.Location = new System.Drawing.Point(188, 314);
+            this.txtAnStudiu.Name = "txtAnStudiu";
+            this.txtAnStudiu.Size = new System.Drawing.Size(84, 27);
+            this.txtAnStudiu.TabIndex = 10;
+            // 
+            // lblNrPrezentare
+            // 
+            this.lblNrPrezentare.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblNrPrezentare.AutoSize = true;
+            this.lblNrPrezentare.Font = new System.Drawing.Font("Georgia", 12F);
+            this.lblNrPrezentare.Location = new System.Drawing.Point(591, 186);
+            this.lblNrPrezentare.Name = "lblNrPrezentare";
+            this.lblNrPrezentare.Size = new System.Drawing.Size(138, 24);
+            this.lblNrPrezentare.TabIndex = 11;
+            this.lblNrPrezentare.Text = "Nr. prezentare";
+            // 
+            // txtNrPrezentare
+            // 
+            this.txtNrPrezentare.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNrPrezentare.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F);
+            this.txtNrPrezentare.Location = new System.Drawing.Point(735, 186);
+            this.txtNrPrezentare.Name = "txtNrPrezentare";
+            this.txtNrPrezentare.Size = new System.Drawing.Size(84, 27);
+            this.txtNrPrezentare.TabIndex = 12;
+            // 
+            // lblData
+            // 
+            this.lblData.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblData.AutoSize = true;
+            this.lblData.Font = new System.Drawing.Font("Georgia", 12F);
+            this.lblData.Location = new System.Drawing.Point(591, 249);
+            this.lblData.Name = "lblData";
+            this.lblData.Size = new System.Drawing.Size(52, 24);
+            this.lblData.TabIndex = 13;
+            this.lblData.Text = "Data";
+            // 
+            // dtpData
+            // 
+            this.dtpData.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dtpData.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F);
+            this.dtpData.Location = new System.Drawing.Point(649, 247);
+            this.dtpData.Name = "dtpData";
+            this.dtpData.Size = new System.Drawing.Size(170, 27);
+            this.dtpData.TabIndex = 14;
+            // 
+            // lblNota
+            // 
+            this.lblNota.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblNota.AutoSize = true;
+            this.lblNota.Font = new System.Drawing.Font("Georgia", 12F);
+            this.lblNota.Location = new System.Drawing.Point(590, 313);
+            this.lblNota.Name = "lblNota";
+            this.lblNota.Size = new System.Drawing.Size(53, 24);
+            this.lblNota.TabIndex = 15;
+            this.lblNota.Text = "Nota";
+            // 
+            // txtNotaStudent
+            // 
+            this.txtNotaStudent.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNotaStudent.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNotaStudent.Location = new System.Drawing.Point(649, 313);
+            this.txtNotaStudent.Name = "txtNotaStudent";
+            this.txtNotaStudent.Size = new System.Drawing.Size(84, 27);
+            this.txtNotaStudent.TabIndex = 16;
+            this.txtNotaStudent.MouseLeave += new System.EventHandler(this.txtNotaStudent_MouseLeave);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pictureBox1.Image = global::Proiect_final_MTP.Properties.Resources.icons8_registration_form_100;
+            this.pictureBox1.Location = new System.Drawing.Point(417, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(109, 103);
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // cmbDiscipline
+            // 
+            this.cmbDiscipline.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.cmbDiscipline.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F);
+            this.cmbDiscipline.FormattingEnabled = true;
+            this.cmbDiscipline.Location = new System.Drawing.Point(188, 250);
+            this.cmbDiscipline.Name = "cmbDiscipline";
+            this.cmbDiscipline.Size = new System.Drawing.Size(281, 28);
+            this.cmbDiscipline.TabIndex = 35;
+            // 
+            // btnAdaugareNota
+            // 
+            this.btnAdaugareNota.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAdaugareNota.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(150)))), ((int)(((byte)(136)))));
+            this.btnAdaugareNota.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAdaugareNota.FlatAppearance.BorderSize = 2;
+            this.btnAdaugareNota.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdaugareNota.Font = new System.Drawing.Font("Lucida Bright", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdaugareNota.ForeColor = System.Drawing.Color.Snow;
+            this.btnAdaugareNota.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.btnAdaugareNota.IconColor = System.Drawing.Color.Black;
+            this.btnAdaugareNota.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnAdaugareNota.Location = new System.Drawing.Point(380, 382);
+            this.btnAdaugareNota.Name = "btnAdaugareNota";
+            this.btnAdaugareNota.Size = new System.Drawing.Size(181, 51);
+            this.btnAdaugareNota.TabIndex = 37;
+            this.btnAdaugareNota.Text = "Adaugare nota";
+            this.btnAdaugareNota.UseVisualStyleBackColor = false;
+            this.btnAdaugareNota.Click += new System.EventHandler(this.btnAdaugareNota_Click);
+            // 
+            // AdaugareNota
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.Controls.Add(this.btnAdaugareNota);
+            this.Controls.Add(this.cmbDiscipline);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtNotaStudent);
+            this.Controls.Add(this.lblNota);
+            this.Controls.Add(this.dtpData);
+            this.Controls.Add(this.lblData);
+            this.Controls.Add(this.txtNrPrezentare);
+            this.Controls.Add(this.lblNrPrezentare);
+            this.Controls.Add(this.txtAnStudiu);
+            this.Controls.Add(this.lblAnStudiu);
+            this.Controls.Add(this.lblDisciplina);
+            this.Controls.Add(this.cmbNrLegitimatie);
+            this.Controls.Add(this.lblLegitimatie);
+            this.Name = "AdaugareNota";
+            this.Size = new System.Drawing.Size(962, 528);
+            this.Load += new System.EventHandler(this.AdaugareNota_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLegitimatie;
+        private System.Windows.Forms.ComboBox cmbNrLegitimatie;
+        private System.Windows.Forms.Label lblDisciplina;
+        private System.Windows.Forms.Label lblAnStudiu;
+        private System.Windows.Forms.TextBox txtAnStudiu;
+        private System.Windows.Forms.Label lblNrPrezentare;
+        private System.Windows.Forms.TextBox txtNrPrezentare;
+        private System.Windows.Forms.Label lblData;
+        private System.Windows.Forms.DateTimePicker dtpData;
+        private System.Windows.Forms.Label lblNota;
+        private System.Windows.Forms.TextBox txtNotaStudent;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox cmbDiscipline;
+        private FontAwesome.Sharp.IconButton btnAdaugareNota;
+    }
+}
---
+++ Proiect final-MTP/AdaugareNota.cs
@@ -0,0 +1,156 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Proiect_final_MTP
+{
+    public partial class AdaugareNota : UserControl
+    {
+
+        MySqlConnection sqlConnection = Connection.getSqlConnection();
+
+        public AdaugareNota()
+        {
+            InitializeComponent();
+        }
+
+
+        // incarcare Form
+        private void AdaugareNota_Load(object sender, EventArgs e)
+        {
+            dtpData.Format = DateTimePickerFormat.Custom;
+            dtpData.CustomFormat = "yyyy-MM-dd";
+            disableButtonAdd();
+
+            try
+            {
+                sqlConnection.Open();
+
+                // selecteaza numarul de legitimatii din tabela de studenti
+                string queryNrLegitimatie =
+                    " SELECT nr_legitimatie" +
+                    " FROM studenti" +
+                    " ORDER BY nr_legitimatie ";
+
+                // selecteaza disciplinele din tabela note
+                string queryDiscipline =
+                    " SELECT DISTINCT disciplina" +
+                    " FROM note" +
+                    " ORDER BY an_studiu," +
+                    "          disciplina";
+
+
+                MySqlDataAdapter dataAdapter = new MySqlDataAdapter();
+                dataAdapter.SelectCommand = new MySqlCommand();
+                dataAdapter.SelectCommand.Connection = sqlConnection;
+                dataAdapter.SelectCommand.CommandText = queryNrLegitimatie;
+
+                DataTable dataTableNrLegitimatie = new DataTable();
+                DataTable dataTableDiscipline = new DataTable();
+
+                dataAdapter.Fill(dataTableNrLegitimatie);
+
+                // adaugare in combobox-ul cmbNrLegitimatie nr. de legitimatie pentru fiecare student din BD
+                for (int i = 0; i < dataTableNrLegitimatie.Rows.Count; i++)
+                {
+                    cmbNrLegitimatie.Items.Add(dataTableNrLegitimatie.Rows[i]["nr_legitimatie"]);
+                }
+
+
+                dataAdapter.SelectCommand = new MySqlCommand();
+                dataAdapter.SelectCommand.Connection = sqlConnection;
+                dataAdapter.SelectCommand.CommandText = queryDiscipline;
+
+                dataAdapter.Fill(dataTableDiscipline);
+
+                // adaugare in combobox-ul cmbDiscipline disciplinele din BD
+                for (int i = 0; i < dataTableDiscipline.Rows.Count; i++)
+                {
+                    cmbDiscipline.Items.Add(dataTableDiscipline.Rows[i]["disciplina"]);
+                }
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            sqlConnection.Close();
+        }
+
+
+        // buton pentru adaugarea unei note unui student in BD
+        private void btnAdaugareNota_Click(object sender, EventArgs e)
+        {
+            bool succes = false;
+
+            try
+            {
+                //  query pentru a introduce o nota in tabela Note
+                string query =
+                "INSERT INTO `note` (`nr_legitimatie`, `disciplina`, `an_studiu`, `nr_prezentare`, `data`, `nota`) " +
+                "VALUES (@nr_legitimatie, @disciplina, @an_studiu, @nr_prezentare, @data, @nota);";
+
+                sqlConnection.Open();
+                MySqlCommand sqlCommand = sqlConnection.CreateCommand();
+                sqlCommand.Parameters.AddWithValue("@nr_legitimatie", cmbNrLegitimatie.Text);
+                sqlCommand.Parameters.AddWithValue("@disciplina", cmbDiscipline.Text);
+                sqlCommand.Parameters.AddWithValue("@an_studiu", Int16.Parse(txtAnStudiu.Text));
+                sqlCommand.Parameters.AddWithValue("@nr_prezentare", Int16.Parse(txtNrPrezentare.Text));
+                sqlCommand.Parameters.AddWithValue("@data", dtpData.Text);
+                sqlCommand.Parameters.AddWithValue("@nota", Double.Parse(txtNotaStudent.Text));
+
+                sqlCommand.CommandText = query;
+                if (sqlCommand.ExecuteNonQuery() > 0)
+                {
+                    MessageBox.Show("Nota a fost adaugata cu succes!", "Succes", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    succes = true;
+                }
+                else
+                {
+                    MessageBox.Show("Nota nu  a fost adaugata in BD, mai incercati!", "Atentionare", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            sqlConnection.Close();
+
+
+            if (succes)
+            {
+                disableButtonAdd();
+                cmbNrLegitimatie.Text = null;
+                cmbDiscipline.Text = null;
+                txtAnStudiu.Text = "";
+                txtNrPrezentare.Text = "";
+                dtpData.Text = null;
+                txtNotaStudent.Text = "";
+            }
+        }
+
+
+        // event dupa ce este luat cursorul mouse-ului 
+        // de pe textbox-ul in care se introduce nota
+        private void txtNotaStudent_MouseLeave(object sender, EventArgs e)
+        {
+            if (txtNotaStudent.Text != string.Empty && txtAnStudiu.Text != string.Empty && txtNrPrezentare.Text != string.Empty)
+            {
+                btnAdaugareNota.Enabled = true;
+            }
+        }
+
+
+        // metoda prin care butonul de adaugare se dezactiveaza
+        private void disableButtonAdd()
+        {
+            if (txtAnStudiu.Text == string.Empty && txtNrPrezentare.Text == string.Empty && txtNotaStudent.Text == string.Empty)
+            {
+                btnAdaugareNota.Enabled = false;
+            }
+        }
+
+    }
+}
