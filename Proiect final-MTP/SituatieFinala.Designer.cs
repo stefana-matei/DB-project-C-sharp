1ce308b194431cd31f54e402f2d18fa43e35a94b
Stefana Matei <stefana.otilia.matei@gmail.com>
2022-01-08 16:41:35 +0200

    Update SituatieFinala UserControl

--- Proiect final-MTP/SituatieFinala.Designer.cs
+++ Proiect final-MTP/SituatieFinala.Designer.cs
@@ -29,10 +29,132 @@ namespace Proiect_final_MTP
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgvSituatieFinala = new System.Windows.Forms.DataGridView();
+            this.dgvMedii = new System.Windows.Forms.DataGridView();
+            this.lblMedii = new System.Windows.Forms.Label();
+            this.cmbAnUniversitar = new System.Windows.Forms.ComboBox();
+            this.lblNoteFinale = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSituatieFinala)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMedii)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvSituatieFinala
+            // 
+            this.dgvSituatieFinala.AllowUserToAddRows = false;
+            this.dgvSituatieFinala.AllowUserToDeleteRows = false;
+            this.dgvSituatieFinala.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvSituatieFinala.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvSituatieFinala.BackgroundColor = System.Drawing.Color.Azure;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(194)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Snow;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvSituatieFinala.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvSituatieFinala.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSituatieFinala.EnableHeadersVisualStyles = false;
+            this.dgvSituatieFinala.Location = new System.Drawing.Point(172, 154);
+            this.dgvSituatieFinala.MultiSelect = false;
+            this.dgvSituatieFinala.Name = "dgvSituatieFinala";
+            this.dgvSituatieFinala.RowHeadersVisible = false;
+            this.dgvSituatieFinala.RowHeadersWidth = 51;
+            this.dgvSituatieFinala.RowTemplate.Height = 24;
+            this.dgvSituatieFinala.Size = new System.Drawing.Size(1046, 208);
+            this.dgvSituatieFinala.TabIndex = 24;
+            // 
+            // dgvMedii
+            // 
+            this.dgvMedii.AllowUserToAddRows = false;
+            this.dgvMedii.AllowUserToDeleteRows = false;
+            this.dgvMedii.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvMedii.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvMedii.BackgroundColor = System.Drawing.Color.Azure;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(194)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Snow;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvMedii.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvMedii.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMedii.EnableHeadersVisualStyles = false;
+            this.dgvMedii.Location = new System.Drawing.Point(172, 460);
+            this.dgvMedii.MultiSelect = false;
+            this.dgvMedii.Name = "dgvMedii";
+            this.dgvMedii.RowHeadersVisible = false;
+            this.dgvMedii.RowHeadersWidth = 51;
+            this.dgvMedii.RowTemplate.Height = 24;
+            this.dgvMedii.Size = new System.Drawing.Size(658, 51);
+            this.dgvMedii.TabIndex = 25;
+            // 
+            // lblMedii
+            // 
+            this.lblMedii.AutoSize = true;
+            this.lblMedii.Font = new System.Drawing.Font("Lucida Bright", 13.8F, System.Drawing.FontStyle.Bold);
+            this.lblMedii.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblMedii.Location = new System.Drawing.Point(115, 415);
+            this.lblMedii.Name = "lblMedii";
+            this.lblMedii.Size = new System.Drawing.Size(79, 27);
+            this.lblMedii.TabIndex = 27;
+            this.lblMedii.Text = "Medii";
+            this.lblMedii.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // cmbAnUniversitar
+            // 
+            this.cmbAnUniversitar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cmbAnUniversitar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbAnUniversitar.FormattingEnabled = true;
+            this.cmbAnUniversitar.Location = new System.Drawing.Point(120, 89);
+            this.cmbAnUniversitar.Name = "cmbAnUniversitar";
+            this.cmbAnUniversitar.Size = new System.Drawing.Size(50, 37);
+            this.cmbAnUniversitar.TabIndex = 29;
+            this.cmbAnUniversitar.SelectedIndexChanged += new System.EventHandler(this.cmbAnUniversitar_SelectedIndexChanged);
+            // 
+            // lblNoteFinale
+            // 
+            this.lblNoteFinale.AutoSize = true;
+            this.lblNoteFinale.Font = new System.Drawing.Font("Lucida Bright", 13.8F, System.Drawing.FontStyle.Bold);
+            this.lblNoteFinale.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblNoteFinale.Location = new System.Drawing.Point(115, 59);
+            this.lblNoteFinale.Name = "lblNoteFinale";
+            this.lblNoteFinale.Size = new System.Drawing.Size(310, 27);
+            this.lblNoteFinale.TabIndex = 28;
+            this.lblNoteFinale.Text = "Selectati anul universitar";
+            this.lblNoteFinale.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // SituatieFinala
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.Controls.Add(this.cmbAnUniversitar);
+            this.Controls.Add(this.lblNoteFinale);
+            this.Controls.Add(this.lblMedii);
+            this.Controls.Add(this.dgvMedii);
+            this.Controls.Add(this.dgvSituatieFinala);
+            this.Name = "SituatieFinala";
+            this.Size = new System.Drawing.Size(1378, 600);
+            this.Load += new System.EventHandler(this.SituatieFinala_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSituatieFinala)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMedii)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvSituatieFinala;
+        private System.Windows.Forms.DataGridView dgvMedii;
+        private System.Windows.Forms.Label lblMedii;
+        private System.Windows.Forms.ComboBox cmbAnUniversitar;
+        private System.Windows.Forms.Label lblNoteFinale;
     }
 }
--- Proiect final-MTP/SituatieFinala.cs
+++ Proiect final-MTP/SituatieFinala.cs
@@ -1,20 +1,125 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Proiect_final_MTP
 {
     public partial class SituatieFinala : UserControl
     {
+        MySqlConnection sqlConnection = Connection.getSqlConnection();
+
         public SituatieFinala()
         {
             InitializeComponent();
+        }
+
+
+        // incarcare Form
+        private void SituatieFinala_Load(object sender, EventArgs e)
+        {
+            incarcareAniUniversitari();
+            afisareMedii();
+        }
+
+
+        // incarcare in combobox a anilor universitari ai studentului
+        private void incarcareAniUniversitari()
+        {
+            try
+            {
+                sqlConnection.Open();
+
+                // selecteaza anii de studiu pentru studentul conectat din tabela note
+                string queryAnStudiu =
+                    " SELECT DISTINCT an_studiu" +
+                    " FROM note" +
+                    " WHERE nr_legitimatie = '" + Student.Legitimatie + "'";
+
+
+                MySqlDataAdapter dataAdapter = new MySqlDataAdapter();
+                dataAdapter.SelectCommand = new MySqlCommand();
+                dataAdapter.SelectCommand.Connection = sqlConnection;
+                dataAdapter.SelectCommand.CommandText = queryAnStudiu;
+
+                DataTable dataTable = new DataTable();
+
+                dataAdapter.Fill(dataTable);
+
+                // adaugare in combobox-ul cmbNrLegitimatie nr. de legitimatie pentru fiecare student din BD
+                for (int i = 0; i < dataTable.Rows.Count; i++)
+                {
+                    cmbAnUniversitar.Items.Add(dataTable.Rows[i]["an_studiu"]);
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            sqlConnection.Close();
+        }
+
+
+        // afisare mediile studentului
+        private void afisareMedii()
+        {
+            string query =
+                " SELECT *" +
+                " FROM medii" +
+                " WHERE nr_legitimatie = '" + Student.Legitimatie + "'";
+
+            connectToDataBase(query, dgvMedii);
+        }
+
+
+        // afisare note finale a studentului in functie de anul selectat
+        private void cmbAnUniversitar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblNoteFinale.Text = "Note finale ale disciplinelor din anul " + cmbAnUniversitar.Text;
+
+            string query =
+                    " SELECT nr_legitimatie," +
+                    "        disciplina," +
+                    "        an_studiu," +
+                    "        MAX(note.nota) AS nota_finala " +
+                    " FROM note " +
+                    " WHERE note.nr_legitimatie = '" + Student.Legitimatie + "'" +
+                    "   AND note.an_studiu = '" + Int16.Parse(cmbAnUniversitar.Text) + "'" +
+                    " GROUP BY note.disciplina" +
+                    " ORDER BY note.disciplina";
+
+            connectToDataBase(query, dgvSituatieFinala);
+        }
+
+
+        // metoda pentru a afisa in DataGridView datele din BD
+        private void connectToDataBase(string query, DataGridView dataGridView)
+        {
+            try
+            {
+                sqlConnection.Open();
+
+                MySqlDataAdapter dataAdapter = new MySqlDataAdapter();
+                dataAdapter.SelectCommand = new MySqlCommand(query, sqlConnection);
+
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+
+                BindingSource bindingSource = new BindingSource();
+                bindingSource.DataSource = dataTable;
+
+                dataGridView.DataSource = bindingSource;
+
+                dataTable.Dispose();
+                dataAdapter.Dispose();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            sqlConnection.Close();
         }
     }
 }
